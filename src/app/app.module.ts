56ff536d02786d4cbfeccd6b7d9c6c62d9fcd1bd
Symeon Tsitoglou <simtsit@gmail.com>
2018-06-17 07:47:49 +0200

    Pages will be constructed by components and data will be now in a service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TableComponent } from './table/table.component';
+import { CardsComponent } from './cards/cards.component';
 
+import { PeopleService } from './people.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     NavbarComponent,
-    TableComponent
+    TableComponent,
+    CardsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PeopleService } from './../people.service';
+
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+
+	peopleData;
+	peopleTotal;
+	
+	constructor(private people: PeopleService) { }
+	ngOnInit() {
+		this.peopleData = this.people.getPeople();
+		this.peopleTotal = this.peopleData.length;
+	}
+	
+
+}
