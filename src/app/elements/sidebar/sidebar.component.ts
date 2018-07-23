b2af3de5cdac9465b6b3a5139d966368ff345621
Symeon Tsitoglou <simtsit@gmail.com>
2018-07-23 22:23:54 +0200

    Side nav rebuilded. Fixed responsive behaviour.

--- src/app/elements/sidebar/sidebar.component.ts
+++ src/app/elements/sidebar/sidebar.component.ts
@@ -13,24 +13,16 @@ export class SidebarComponent implements OnInit {
 			if(this.toggleOn) {
 				this.toggleOn = false;
 				$('.menuItemLabel').addClass('toggleOff');
-				$('.logo').addClass('toggleOff');
-				$('.sidebarWrapper').addClass('sidebarWrapperToggled');
-				$('.container').addClass('containerToggled');
+				$('.nav-side-menu').addClass('nav-side-menu-toggled');
 				document.getElementById('toggleButton').classList.remove('fa-angle-left');
 				document.getElementById('toggleButton').classList.add('fa-angle-right');
-				$('.logo').addClass('toggleOff');
-				$('.logoMini').removeClass('toggleOff');
 
 			} else {
 					this.toggleOn = true;
 				$('.menuItemLabel').removeClass('toggleOff');
-				$('.logo').removeClass('toggleOff');
-				$('.sidebarWrapper').removeClass('sidebarWrapperToggled');
-				$('.container').removeClass('containerToggled');
+				$('.nav-side-menu').removeClass('nav-side-menu-toggled');
 				document.getElementById('toggleButton').classList.add('fa-angle-left');
 				document.getElementById('toggleButton').classList.remove('fa-angle-right');
-				$('.logo').removeClass('toggleOff');
-				$('.logoMini').addClass('toggleOff');					
 			}
 
 	}
